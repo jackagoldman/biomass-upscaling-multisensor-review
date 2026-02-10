5d75f7b4fb8d9c5d3e0120fc58e87c6d452d31f8
jackagoldman <jandrewgoldman@gmail.com>
2026-02-10 18:47:47 -0500

    Add Scopus API integration script and install required dependencies

---
+++ R/scopus.R
@@ -0,0 +1,64 @@
+# modules for working with scopus api
+
+#' check scopus api key
+#'
+#' @returns
+#'
+#' @export
+#' @examples check_scopus_api_key()
+check_scopus_api_key <- function() { 
+  if (isTRUE(rscopus::is_elsevier_authorized())) {
+    print("Scopus API key is set.")
+  } else {
+    print("Setting Scopus API key...")
+    scopus_api_key <- config::get("scopus-api-key")
+    rscopus::set_api_key(scopus_api_key)
+    if (isTRUE(rscopus::is_elsevier_authorized())) {
+      print("Scopus API key is now set.")
+    } 
+  }
+}
+
+#' TODO: add function to search scopus and return dataframe of results
+#'
+#' @param query
+#' @param ...
+#'
+#' @returns
+#'
+#' @export
+#' @examples test <- search_scopus(query = config::get("query"), start =0, count = 25)
+search_scopus <- function(query, count = 25, start = 0) {
+   res = rscopus::scopus_search(query = query, view = "STANDARD", count = count, start = start)
+    df = rscopus::gen_entries_to_df(res$entries)
+    head(df$df)
+  return(df)
+}
+
+
+#' Title
+#'
+#' @param eid
+#'
+#' @returns
+#'
+#' @export
+#' @examples
+get_scopus_abstract <- function(eid) {
+  res = rscopus::abstract_retrieval(id = eid, identifier = "eid", view = "FULL")
+  return(res$abstract)
+}
+
+#' Title
+#'
+#' @param df
+#'
+#' @returns
+#'
+#' @export
+#' @examples abtracts  <- process_abstracts(test$df)
+process_abstracts <- function(df) {
+  df$abstract <- sapply(df$eid, get_scopus_abstract)
+  return(df)
+}
+
